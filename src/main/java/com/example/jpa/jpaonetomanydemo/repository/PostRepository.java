5e1649e356ca889f52b885aaf93bb195cb007148
Edwin Dominguez <edwindominguez@hotmail.com>
2019-10-30 19:53:13 -0500

    add rest functionality

--- src/main/java/com/example/jpa/jpaonetomanydemo/repository/PostRepository.java
+++ src/main/java/com/example/jpa/jpaonetomanydemo/repository/PostRepository.java
@@ -1,4 +1,9 @@
 package com.example.jpa.jpaonetomanydemo.repository;
 
-public interface PostRepository {
+import com.example.jpa.jpaonetomanydemo.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
